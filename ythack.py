347b0b62316949313b1cfaee362a8369d788bb22
cololoramara <106280766+cololoramara@users.noreply.github.com>
2022-05-25 23:24:30 +0200

    ythack
    le logiciel pour pirater

---
+++ ythack.py
@@ -0,0 +1,13 @@
+input("enter the channel you want to hack:")
+
+input("password hack click for now he or she password")
+
+input(" password : get in 4k")
+
+input("just do not hack any account")
+
+input ("thank you . click")
+
+input (" now close pls")
+
+input("I SAID CLOSE NOW CLOSE OR IM GONNA HACK YOU HEHE")
